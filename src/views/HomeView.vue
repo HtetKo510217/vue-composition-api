4905175e4aacc3f94376650ca8358a91d8b9e7c0
htetko <htetko.connetis@gmail.com>
2023-01-03 16:56:40 +0630

    lifecycle hook in composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <h1>home page</h1>
-    <PostList :posts = "posts"></PostList>
+    <div v-if="showPost">
+      <PostList :posts = "posts"></PostList>
+    </div>
+    <button @click="showPost = !showPost">Toggle Post</button> <!-- test onUnmounted -->
+    <button @click="posts.pop()">Delete post</button> <!-- test onUpdated -->
   </div>
 </template>
 
@@ -12,6 +16,7 @@ import { ref } from '@vue/reactivity'
 export default {
   components: { PostList },
   setup() {
+   let showPost = ref(true)
    let posts = ref([
     {title:'title 1',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo non quibusdam expedita placeat obcaecati saepe in doloribus quasi cum nihil commodi optio, itaque ratione perferendis sed modi consectetur blanditiis omnis?',id:1},
     {title:'title 2',body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo non quibusdam expedita placeat obcaecati saepe in doloribus quasi cum nihil commodi optio, itaque ratione perferendis sed modi consectetur blanditiis omnis?',id:2},
@@ -21,7 +26,7 @@ export default {
    ])
 
 
-   return {posts}
+   return {posts,showPost}
   }
 }
 </script>
